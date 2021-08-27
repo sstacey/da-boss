9b252fa52a83b12ebdc6755719dfa597fc2f8374
sstacey <shaun.stacey1@gmail.com>
2021-08-27 11:48:52 -0500

    Show messages when user not found or bad password

--- config/passport.js
+++ config/passport.js
@@ -23,62 +23,56 @@ passport.use(
         })
         return done(null, newUser)
       } catch (e) {
-        return done(e.message)
+        done(e)
       }
     }
   )
 )
 
-// passport.use(
-//   'login',
-//   new localStrategy(
-//     {
-//       usernameField: 'email',
-//       passwordField: 'password',
-//     },
-//     async (email, password, done) => {
-//       try {
-//         // const { rows } = await db.query(
-//         //   'SELECT * FROM users WHERE email = $1',
-//         //   [email]
-//         // )
-//         // const user = rows[0]
-//         const users = await knex('users').where('email', email)
-//         user = users[0]
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ where: { email } })
 
-//         if (!user) {
-//           return done(null, false, { message: 'User not found' })
-//         }
+        if (!user) {
+          return done(null, false, { message: 'User not found' })
+        }
 
-//         const validate = await bcrypt.compare(password, user.password)
+        const validate = await bcrypt.compare(password, user.password)
 
-//         if (!validate) {
-//           return done(null, false, { message: 'Wrong Password' })
-//         }
+        if (!validate) {
+          return done(null, false, { message: 'Wrong Password' })
+        }
 
-//         return done(null, user, { message: 'Logged in Successfully' })
-//       } catch (error) {
-//         return done(error)
-//       }
-//     }
-//   )
-// )
+        return done(null, user, { message: 'Logged in Successfully' })
+      } catch (error) {
+        return done(error)
+      }
+    }
+  )
+)
 
-// const JWTstrategy = require('passport-jwt').Strategy
-// const ExtractJWT = require('passport-jwt').ExtractJwt
+const JWTstrategy = require('passport-jwt').Strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt
 
-// passport.use(
-//   new JWTstrategy(
-//     {
-//       secretOrKey: process.env.JWT_SECRET,
-//       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken('secret_token'),
-//     },
-//     async (token, done) => {
-//       try {
-//         return done(null, token.user)
-//       } catch (error) {
-//         done(error)
-//       }
-//     }
-//   )
-// )
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken('secret_token'),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user)
+      } catch (error) {
+        done(error)
+      }
+    }
+  )
+)
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const passport = require('passport')
+const jwt = require('jsonwebtoken')
 
 router.post(
   '/signup',
@@ -11,5 +12,26 @@ router.post(
     })
   }
 )
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        return res.status(500).send({ message: info.message, error: err })
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error)
+
+        const body = { id: user.id, email: user.email }
+        const token = jwt.sign({ user: body }, process.env.JWT_SECRET)
+
+        return res.json({ token })
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })(req, res, next)
+})
 
 module.exports = router
